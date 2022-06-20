1151b5fc24144cf1da4f9782293a2f46f0d6e9a6
Asfandyar Sheikh <andy@replyqa.com>
2022-06-21 00:46:39 +0500

    woohoo

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,5 @@
 from tif_util import process_template, process_yaml
 
 if __name__ == '__main__':
-    template = process_yaml(app, 'template')
+    template = process_yaml('app', 'template')
     process_template(template)
