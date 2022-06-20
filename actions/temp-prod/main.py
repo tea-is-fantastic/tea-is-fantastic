8ab10edd2d927c5383d2a9036d7a5a07aab6bea4
Asfandyar Sheikh <andy@replyqa.com>
2022-06-21 03:22:15 +0500

    woohoo

--- actions/temp-prod/main.py
+++ actions/temp-prod/main.py
@@ -4,4 +4,6 @@ from distutils.dir_util import copy_tree
 TEMP_PATH = os.environ['TEMP_PATH']
 OUTPUT_PATH = os.environ['OUTPUT_PATH']
 
-copy_tree(TEMP_PATH, OUTPUT_PATH)
+if __name__ == '__main__':
+    os.rename(TEMP_PATH, OUTPUT_PATH)
+    os.mkdir(TEMP_PATH)
