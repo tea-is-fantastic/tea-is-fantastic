e5a08df100a6342df6f018855fad53de1279a66e
Asfandyar Sheikh <andy@replyqa.com>
2022-06-21 03:07:19 +0500

    woohoo

--- actions/expo-init/main.py
+++ actions/expo-init/main.py
@@ -5,4 +5,4 @@ TEMP_PATH = sys.argv[2]
 
 if __name__ == '__main__':
     os.chdir(TEMP_PATH)
-    os.system('expo init . --name {} --no-install --yes'.format(name))
+    os.system('expo init {} --no-install --yes'.format(name))
