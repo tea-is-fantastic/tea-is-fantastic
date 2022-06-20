970792bbbec1870369df388a0d26ea54be8f0361
Asfandyar Sheikh <andy@replyqa.com>
2022-06-21 01:42:37 +0500

    woohoo

--- actions/expo-init/main.py
+++ actions/expo-init/main.py
@@ -1,8 +1,8 @@
-import os, sys
+import os, sys, subprocess
 
 name = sys.argv[1]
 TEMP_PATH = sys.argv[2]
 
 if __name__ == '__main__':
     os.chdir(TEMP_PATH)
-    os.system('expo init . --name {} --no-install --yes'.format(name))
+    subprocess.call(['expo', 'init' '.' '--name', name, '--no-install', '--yes'])
