2b14ac5062ac2e1d6bdfa9d16fbc20a1a506f935
Asfandyar Sheikh <andy@replyqa.com>
2022-06-21 18:46:00 +0500

    woohoo

--- actions/github-push/main.py
+++ actions/github-push/main.py
@@ -8,16 +8,22 @@ REPO_PATH = f"https://{creds}@github.com/{repo}"
 
 
 if __name__ == '__main__':
-    repo = git.Repo.init(OUTPUT_PATH)
+    os.chdir(OUTPUT_PATH)
+    os.system("git init .")
+    os.system(f'git remote add origin {REPO_PATH}')
+    os.system("git add -A")
+    os.system('git commit -am "woohoo"')
+    os.system("git push --all -f")
+    # repo = git.Repo.init(OUTPUT_PATH)
     
-    for path, subdirs, files in os.walk(OUTPUT_PATH):
-        for name in files:
-            repo.index.add(os.path.join(path, name))
+    # for path, subdirs, files in os.walk(OUTPUT_PATH):
+    #     for name in files:
+    #         repo.index.add(os.path.join(path, name))
 
-    repo.index.commit("initial commit")
-    origin = repo.create_remote('origin', REPO_PATH)
-    # repo.create_head('master', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-    print("START")
-    origin.push(force=True, all=True).raise_if_error()
-    print("FINISH")
+    # repo.index.commit("initial commit")
+    # origin = repo.create_remote('origin', REPO_PATH)
+    # # repo.create_head('master', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    # print("START")
+    # origin.push(force=True, all=True).raise_if_error()
+    # print("FINISH")
 
--- actions/move-contents/main.py
+++ actions/move-contents/main.py
@@ -4,4 +4,4 @@ SRC_PATH = sys.argv[1]
 DEST_PATH = sys.argv[2]
 
 if __name__ == '__main__':
-    shutil.copytree(SRC_PATH, DEST_PATH, dirs_exist_ok=True)
+    shutil.move(SRC_PATH, DEST_PATH)
