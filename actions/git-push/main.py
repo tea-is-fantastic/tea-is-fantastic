5ecbcd41a80c78a4752a84ad08d496dc427c2d6b
Asfandyar Sheikh <andy@replyqa.com>
2022-06-21 16:01:53 +0500

    woohoo

--- actions/git-push/main.py
+++ actions/git-push/main.py
@@ -16,6 +16,6 @@ if __name__ == '__main__':
     origin = repo.create_remote('origin', OUTPUT_PATH)
     print([r for r in origin.refs])
     # repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
-    repo.checkout()
+    # repo.checkout()
     origin.push(force=True, all=True)
 
