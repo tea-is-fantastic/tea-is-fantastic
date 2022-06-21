591f1770bd478bd8c51812676c6edafc1573a5a2
Asfandyar Sheikh <andy@replyqa.com>
2022-06-21 15:52:55 +0500

    woohoo

--- actions/git-push/main.py
+++ actions/git-push/main.py
@@ -1,19 +1,19 @@
 import os, sys
 import git
 
-REPO_PATH = sys.argv[1]
+INPUT_PATH = sys.argv[1]
 OUTPUT_PATH = sys.argv[2]
 
 
 if __name__ == '__main__':
-    repo = git.Repo.init(OUTPUT_PATH)
+    repo = git.Repo.init(INPUT_PATH)
     
-    for path, subdirs, files in os.walk(OUTPUT_PATH):
+    for path, subdirs, files in os.walk(INPUT_PATH):
         for name in files:
             repo.index.add(os.path.join(path, name))
 
     repo.index.commit("initial commit")
-    origin = repo.create_remote('origin', REPO_PATH)
+    origin = repo.create_remote('origin', OUTPUT_PATH)
     repo.create_head('master', origin.refs.master).set_tracking_branch(origin.refs.master).checkout()
-    origin.push()
+    origin.push(force=True, all=True)
 
