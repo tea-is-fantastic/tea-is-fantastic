d3897c156cbe7fb76916a0e976e51111dae80b4d
Asfandyar Sheikh <andy@replyqa.com>
2022-06-23 04:36:17 +0500

    woohoo

--- actions/expo-init/main.py
+++ actions/expo-init/main.py
@@ -5,4 +5,4 @@ TEMP_PATH = sys.argv[2]
 
 if __name__ == '__main__':
     os.chdir(TEMP_PATH)
-    os.system('expo init {} --no-install --yes'.format(name))
+    os.system(f'expo init {name} -t expo-template-blank-typescript --no-install')
