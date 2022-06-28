2f87ebdeda52086b8b7af1c861996ff19dd3d362
Asfandyar Sheikh <andy@replyqa.com>
2022-06-28 23:28:12 +0500

    woohoo

--- scripts/tif_yaml.py
+++ scripts/tif_yaml.py
@@ -8,9 +8,10 @@ def open_yaml(yaml_loc):
     with open(yaml_loc, "r") as stream:
         return yaml.safe_load(stream)
 
-
-app_yaml = open_yaml(f"{DATA_PATH}/app.yaml")
-assets_yaml = open_yaml(f"{DATA_PATH}/assets.yaml")
+yamls = {
+    "app": open_yaml(f"{DATA_PATH}/app.yaml"),
+    "assets": open_yaml(f"{DATA_PATH}/assets.yaml"),
+}
 
 
 def open_raw(yaml_loc):
@@ -22,13 +23,6 @@ def yaml_dict(stream):
     return yaml.safe_load(stream)
 
 
-def name_to_yaml(name):
-    if name == "assets":
-        return assets_yaml
-    else:
-        return app_yaml
-
-
 def nested_get(dic, keys):
     for key in keys:
         dic = dic[key]
@@ -37,5 +31,5 @@ def nested_get(dic, keys):
 
 def yaml_extract(name, varname):
     keys = varname.split('.')
-    test_yaml = name_to_yaml(name)
+    test_yaml = yamls[name]
     return nested_get(test_yaml, keys)
