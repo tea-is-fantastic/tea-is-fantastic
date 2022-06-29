24a368882c452ad6dd509ec7453f4fc8751d4973
Asfandyar Sheikh <andy@replyqa.com>
2022-06-30 04:00:05 +0500

    woohoo

--- actions/rn-init/main.py
+++ actions/rn-init/main.py
@@ -5,4 +5,4 @@ TEMP_PATH = sys.argv[2]
 
 if __name__ == '__main__':
     os.chdir(TEMP_PATH)
-    os.system(f'npx react-native init {name} --template react-native-template-typescript --no-install --quiet')
+    os.system(f'npx react-native init {name} --template react-native-template-typescript --no-install --yes')
